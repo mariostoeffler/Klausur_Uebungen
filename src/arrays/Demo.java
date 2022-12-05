671a7d5776c7770cb8d99ff0973be2bb6d184629
mariostoeffler <52000392@edu.campus02.at>
2022-12-05 21:36:38 +0100

    began with first two classes;

---
+++ src/arrays/Demo.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        int[] testNumbers = {1, -8, 3, 4, -9};
+
+        NumberHelper.removeDuplicates(testNumbers);
+
+    }
+}
---
+++ src/arrays/NumberHelper.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class NumberHelper {
+
+    public static boolean isSortedArray(int[] numbers) {
+
+
+            if (numbers[1] > numbers[0] && numbers[numbers.length] > numbers[1])
+                return true;
+            else return false;
+    }
+
+    public static int[] removeDuplicates(int[] numbers) {
+        int[] newNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length ; i++) {
+            if (numbers[i] > 0) {
+                newNumbers[i] = numbers[i];
+            }
+            else  {
+                newNumbers[i] = -1;
+            }
+        }
+        return newNumbers;
+    }
+}
